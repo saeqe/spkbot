a9f59b6cee608517090c63aef4ee2cabbaa6ddfc
Mr Ah S <mrahs@emeil.ir>
2016-02-10 19:06:41 +0330

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,10 +91,10 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'megasatan' then -- Put everything you like :)
+  if matches[1]:lower() == 'pride' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /idetergent ")
+    savelog(msg.to.id, name.." ["..msg.from.id.."] used /pride ")
     return about
   end 
   if matches[1]:lower() == "statslist" then
@@ -120,7 +120,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "megasatan" then -- Put everything you like :)
+    if matches[2] == "pride" then -- Put everything you like :)
       if not is_admin(msg) then
         return "For admins only !"
       else
@@ -148,8 +148,8 @@ return {
     "^([Ss]tats)$",
     "^([Ss]tatslist)$",
     "^([Ss]tats) (group) (%d+)",
-    "^([Ss]tats) (megasatan)",-- Put everything you like :)
-		"^([Mm]egasatan)"-- Put everything you like :)
+    "^([Ss]tats) (pride)",-- Put everything you like :)
+		"^([Pp]ride)"-- Put everything you like :)
     }, 
   run = run
 }
